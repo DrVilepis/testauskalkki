74c672f2ea5fcd8f86336954ba1482530c18e664
DrVilepis <63553760+DrVilepis@users.noreply.github.com>
2021-12-05 22:45:44 +0200

    Forgor to add file💀

---
+++ src/eval.rs
@@ -0,0 +1,106 @@
+use std::collections::HashMap;
+
+use libtestauskalkki as ltk;
+use mathstructs::MathElement::Number;
+
+use crate::*;
+
+pub fn eval_math(vars: &mut HashMap<String, Math>, math: Math) -> Option<f64> {
+    //dbg!("{}",&math);
+    let mut math = math.iter();
+    let mut math_buffer = Math::new();
+    while let Some(m) = math.next() {
+        match m {
+            MathElement::Operator(o) if o == &MathOperator::Assign => {
+                let var = math_buffer.pop().unwrap();
+                match var {
+                    MathElement::Variable(s) => {
+                        vars.insert(s, math.clone().map(|e| e.to_owned()).collect());
+                        return None;
+                    }
+                    _ => eprintln!("Skill"),
+                }
+            }
+            _ => math_buffer.push(m.clone()),
+        }
+    }
+    let tmp = math_buffer.clone();
+    math = tmp.iter();
+    math_buffer.clear();
+    while let Some(m) = math.next() {
+        match m {
+            MathElement::Math(_) => {
+                let math = reduce(vars, m);
+                math_buffer.push(mathstructs::MathElement::Number(math));
+            }
+            MathElement::Function(_) => {}
+            _ => math_buffer.push(m.clone()),
+        }
+    }
+    let tmp = math_buffer.clone();
+    math = tmp.iter();
+    math_buffer.clear();
+    while let Some(m) = math.next() {
+        match m {
+            MathElement::Number(_) => math_buffer.push(m.clone()),
+            MathElement::Operator(o) => match o {
+                MathOperator::Multiply | MathOperator::Divide => {
+                    let m1 = math_buffer.pop().unwrap();
+                    math_buffer.push(Number(operate(
+                        o,
+                        reduce(vars, &m1),
+                        reduce(vars, math.next().unwrap()),
+                    )));
+                }
+                _ => math_buffer.push(m.clone()),
+            },
+            MathElement::Function(_) => {}
+            _ => math_buffer.push(m.clone()),
+        }
+    }
+    let tmp = math_buffer.clone();
+    math = tmp.iter();
+    math_buffer.clear();
+    while let Some(m) = math.next() {
+        match m {
+            MathElement::Operator(o) => {
+                let m1 = math_buffer.pop().unwrap();
+                math_buffer.push(Number(operate(
+                    o,
+                    reduce(vars, &m1),
+                    reduce(vars, math.next().unwrap()),
+                )));
+            }
+            _ => math_buffer.push(m.clone()),
+        }
+    }
+    match math_buffer.pop().unwrap() {
+        MathElement::Number(n) => Some(n),
+        MathElement::Variable(v) => Some(reduce(vars, &MathElement::Variable(v))),
+        _ => {
+            eprintln!("Error: Missing skill");
+            None
+        }
+    }
+}
+
+pub fn reduce(vars: &mut HashMap<String, Math>, math: &MathElement) -> f64 {
+    match math {
+        MathElement::Number(n) => n.to_owned(),
+        MathElement::Operator(_o) => {
+            panic!("Skill issue");
+        }
+        MathElement::Math(m) => eval_math(vars, m.clone()).unwrap(),
+        MathElement::Variable(var) => eval_math(vars, vars.get(var).unwrap().to_vec()).unwrap(),
+        _ => todo!("{:?}", math),
+    }
+}
+
+pub fn operate(oper: &MathOperator, n1: f64, n2: f64) -> f64 {
+    match oper {
+        MathOperator::Add => ltk::basic::add(n1, n2),
+        MathOperator::Multiply => ltk::basic::multiply(n1, n2),
+        MathOperator::Divide => ltk::basic::divide(n1, n2),
+        _ => todo!(),
+    }
+}
---
+++ src/mathstructs.rs
@@ -0,0 +1,64 @@
+use std::collections::HashMap;
+
+pub struct MathState {
+    pub vars: HashMap<String, Math>,
+    pub curmath: Option<TS>,
+    pub buffer: String,
+}
+
+impl MathState {
+    pub fn new() -> Self {
+        Self {
+            vars: HashMap::new(),
+            curmath: None,
+            buffer: String::new(),
+        }
+    }
+}
+
+pub type Math = Vec<MathElement>;
+
+#[derive(Clone, Debug)]
+pub enum MathElement {
+    Operator(MathOperator),
+    Number(f64),
+    Math(Math),
+    Function(MathFunction),
+    Variable(String),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum MathOperator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Assign,
+}
+
+pub const OPERATORS: &str = "+-*/=;";
+
+pub fn to_math(s: &str) -> MathOperator {
+    match s {
+        "+" => MathOperator::Add,
+        "-" => MathOperator::Subtract,
+        "*" => MathOperator::Multiply,
+        "/" => MathOperator::Divide,
+        "=" => MathOperator::Assign,
+        _ => panic!("Invalid operator: {}", s),
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct MathFunction {
+    pub func: String,
+    pub args: Math,
+}
+
+pub enum TS {
+    Operator,
+    Number,
+    Math,
+    Function,
+    Variable,
+}
